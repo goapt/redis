d24aaef3a4b444f54782c00660fd212712184bf7
fifsky <fifsky@gmail.com>
2018-12-27 10:21:22 +0800

    add HincrBy

--- base.go
+++ base.go
@@ -69,6 +69,10 @@ func (b *BaseRedis) HExists(key string, field string) (bool, error) {
 	return b.Client().HExists(key, field).Result()
 }
 
+func (b *BaseRedis) HIncrBy(key string, field string,incr int64) (int64, error) {
+	return b.Client().HIncrBy(key, field,incr).Result()
+}
+
 func (b *BaseRedis) Exists(key string) bool {
 	n, err := b.Client().Exists(key).Result()
 

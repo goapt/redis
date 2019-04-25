2352a0b8b82d173d84edd8ff7cca8f2c519458c5
fifsky <fifsky@gmail.com>
2019-04-25 10:17:02 +0800

    map to interface time.Time auto format

--- base_test.go
+++ base_test.go
@@ -23,18 +23,6 @@ type user struct {
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
 
-func (u *user) DbName() string {
-	return "default"
-}
-
-func (u *user) TableName() string {
-	return "user"
-}
-
-func (u *user) PK() string {
-	return "id"
-}
-
 func TestBaseRedis_HGetAll(t *testing.T) {
 	client := NewBaseRedis("default")
 
@@ -51,6 +39,38 @@ func TestBaseRedis_HGetAll(t *testing.T) {
 	}
 
 	u2 := &user{}
+
+	err = client.HGetAll("test",u2)
+
+	if err != nil {
+		t.Errorf("hgetall error:%s",err)
+	}
+
+	fmt.Println(u2)
+}
+
+type user2 struct {
+	Id int `json:"id" db:"id"`
+	Name string `json:"name" db:"name"`
+	CreatedAt string `json:"created_at" db:"created_at"`
+}
+
+func TestBaseRedis_HGetAllString(t *testing.T) {
+	client := NewBaseRedis("default")
+
+	u := &user2{
+		Id:1,
+		Name:"test",
+		CreatedAt:time.Now().Format("2006-01-02 15:04:05"),
+	}
+
+	err := client.HMSet("test",u)
+
+	if err != nil {
+		t.Errorf("hmset error:%s",err)
+	}
+
+	u2 := &user2{}
 
 	err = client.HGetAll("test",u2)
 
